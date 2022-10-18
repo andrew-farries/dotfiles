51fed677a13f81a058d85954eef1f0529970970e
Andrew Farries <andrew@gitpod.io>
2022-10-18 21:30:20 +0100

    Use vi keybindings in fish shell
    But still use emacs style when in insert mode.

--- private_dot_config/fish/functions/fish_user_key_bindings.fish
+++ private_dot_config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,12 @@
 function fish_user_key_bindings
+  #
+  # Use emacs keybindings when in insert mode
+  # See the fish shell docs:
+  # https://fishshell.com/docs/current/interactive.html 
+  #
+
+  fish_default_key_bindings -M insert
+  fish_vi_key_bindings --no-erase insert
+
   fzf_key_bindings
 end
