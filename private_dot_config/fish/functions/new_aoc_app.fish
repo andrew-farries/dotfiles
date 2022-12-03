5b07cde342196df0c07d788fdbfb2ab04d8f8cd7
Andrew Farries <andrew@gitpod.io>
2022-12-03 18:16:33 +0000

    Add new advent of code app function

---
+++ private_dot_config/fish/functions/new_aoc_app.fish
@@ -0,0 +1,17 @@
+function new_aoc_app --description "create a new skeleton advent of code application"
+  set destination $argv[1]
+
+  if test -z "$destination"
+    set destination .
+  end
+
+  if ! test -d $destination
+    mkdir -p $destination
+  end
+
+  git clone git@github.com:andrew-farries/skeleton-aoc-app.git $destination
+  pushd $destination
+  npm install 
+  rm -rf .git/
+  popd
+end
