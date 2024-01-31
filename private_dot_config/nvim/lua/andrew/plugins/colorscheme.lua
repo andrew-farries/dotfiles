2d2152e9bdc8f5506517f80adb02b46aeb3537a7
Andrew Farries <andrew.farries@xata.io>
2024-01-31 21:56:22 +0000

    [lua nvim]: More work on colorschemes

--- private_dot_config/nvim/lua/andrew/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/andrew/plugins/colorscheme.lua
@@ -3,6 +3,17 @@ return {
   lazy=false,
   priority=1000,
   config = function()
+    -- use line window separators
+    vim.g.nightflyWinSeparator = 2
     vim.cmd([[colorscheme nightfly]])
   end,
 }
+
+-- return {
+  -- "kyazdani42/blue-moon",
+  -- lazy=false,
+  -- priority=1000,
+  -- config = function()
+    -- vim.cmd([[colorscheme blue-moon]])
+  -- end,
+-- }
