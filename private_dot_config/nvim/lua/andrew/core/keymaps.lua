df8e8a2a58784d24fc7c0c2ea587c58ba630b1cd
Andrew Farries <andrew.farries@xata.io>
2024-02-09 16:28:23 +0000

    More nvim lua config

--- private_dot_config/nvim/lua/andrew/core/keymaps.lua
+++ private_dot_config/nvim/lua/andrew/core/keymaps.lua
@@ -1,1 +1,9 @@
 -- This is where basic, non plugin related keymaps will go
+
+--
+-- Movement between windows
+--
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-j>", "<C-k>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
