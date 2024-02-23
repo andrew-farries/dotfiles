2e6d0bca4ca407b37326959f9de40eb05134cae2
Andrew Farries <andrew.farries@xata.io>
2024-02-23 17:27:47 +0000

    Add path completion to `nvim-cmp`

--- private_dot_config/nvim/lua/andrew/plugins/nvim-cmp.lua
+++ private_dot_config/nvim/lua/andrew/plugins/nvim-cmp.lua
@@ -3,7 +3,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
---			"hrsh7th/cmp-path",
+    "hrsh7th/cmp-path",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
 --			"rafamadriz/friendly-snippets",
@@ -36,6 +36,7 @@ return {
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
+        { name = 'luasnip' },
         { name = 'buffer', 
           option = {
             get_bufnrs = function()
@@ -43,7 +44,7 @@ return {
             end
           }
         },
-        { name = 'luasnip' },
+        { name = 'path' },
       }),
       sorting = {
         comparators = {
--- private_dot_config/nvim/notes.md
+++ private_dot_config/nvim/notes.md
@@ -3,4 +3,4 @@
 Next:
 * Finish `nvim-cmp` setup.
   * Scroll to the bottom of: https://github.com/hrsh7th/nvim-cmp?tab=readme-ov-file
-  * Need to install a snippet manager to get snippet completions to work.
+  * Add path completions.
