39c9942c329ed83ab901f3c02cad11a519b3fd96
Andrew Farries <andrew.farries@xata.io>
2024-02-23 22:45:04 +0000

    Add <c-c> to nvim config

--- private_dot_config/nvim/lua/andrew/core/keymaps.lua
+++ private_dot_config/nvim/lua/andrew/core/keymaps.lua
@@ -1,14 +1,15 @@
--- This is where basic, non plugin related keymaps will go
+--
+-- Basic, non-plugin specific keymaps
+--
 
---
 -- Movement between windows
---
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus window: left"})
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Focus window: down"})
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Focus window: up"})
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus window: right"})
 
---
 -- Disable space in normal mode
---
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true, desc = "Disable space" })
+
+-- Use <c-c> to clear and close various things
+vim.keymap.set("n", "<C-c>", vim.cmd.nohls, { desc = "Clear search highlights" })
