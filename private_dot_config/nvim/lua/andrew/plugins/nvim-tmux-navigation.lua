04faa7d373c96d571162974aed961ef853fa4887
Andrew Farries <andrew.farries@xata.io>
2024-03-02 15:42:19 +0000

    Add nvim-tmux-navigation plugin

---
+++ private_dot_config/nvim/lua/andrew/plugins/nvim-tmux-navigation.lua
@@ -0,0 +1,10 @@
+return { 'alexghergh/nvim-tmux-navigation', config = function()
+    local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+    vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+    vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+    vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+    vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+
+  end
+}
