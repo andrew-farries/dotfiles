9b5b6911a40873e65740a7dfd8c80658ca9b0e6b
Andrew Farries <andrew.farries@xata.io>
2024-03-25 09:58:11 +0000

    More toggleterm config

--- private_dot_config/nvim/lua/andrew/plugins/toggleterm.lua
+++ private_dot_config/nvim/lua/andrew/plugins/toggleterm.lua
@@ -4,6 +4,17 @@ return {
   config = function()
     require("toggleterm").setup({
       open_mapping = [[<c-t>]],
+      direction = 'float',
+      shell = 'fish',
+      float_opts = {
+        border = 'curved',
+        height = function(term)
+          return math.max(vim.o.lines - 4, 20)
+        end,
+        width = function(term)
+          return math.max(vim.o.columns - 4, 20)
+        end,
+      }
   })
   end,
 }
