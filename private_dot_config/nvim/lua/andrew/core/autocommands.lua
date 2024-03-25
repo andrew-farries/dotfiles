5f3ac601ab1ae9f84e406ade22b6d56e9d36bafd
Andrew Farries <andrew.farries@xata.io>
2024-03-25 12:11:06 +0000

    More autocommand stuff

--- private_dot_config/nvim/lua/andrew/core/autocommands.lua
+++ private_dot_config/nvim/lua/andrew/core/autocommands.lua
@@ -17,7 +17,7 @@ vim.api.nvim_create_autocmd({"InsertLeave", "TextChanged", "FocusLost"}, {
 -- Reload the file on focus gained
 --
 vim.api.nvim_create_autocmd({"FocusGained", "TermLeave"}, {
-  command = "silent edit",
+  command = "silent! edit",
   group = "AndrewAuGroup",
   desc = "Reload the buffer on focus gained",
 })
