a1c0f7d54c56f607bc1349adc0807bffc16a0736
Andrew Farries <andrew.farries@xata.io>
2024-03-31 22:39:11 +0100

    Add initial treesitter textojbects configuration

--- private_dot_config/nvim/lua/andrew/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/andrew/plugins/nvim-treesitter.lua
@@ -1,40 +1,79 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = {
-            "bash",
-            "go",
-            "html",
-            "javascript",
-            "lua",
-            "nix",
-            "rust",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
+    configs.setup({
+        ensure_installed = {
+          "bash",
+          "go",
+          "html",
+          "javascript",
+          "lua",
+          "nix",
+          "rust",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+        },
+        sync_install = false,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "'",
+            node_incremental = "'",
+            scope_incremental = false,
+            node_decremental = "\"",
           },
-          sync_install = false,
-          highlight = {
+        },
+        indent = {
+          enable = false
+        },
+        textobjects = {
+          select = {
             enable = true,
-            additional_vim_regex_highlighting = false,
-          },
-          incremental_selection = {
-            enable = true,
+            lookahead = true,
+
             keymaps = {
-              init_selection = "'",
-              node_incremental = "'",
-              scope_incremental = false,
-              node_decremental = "\"",
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              -- You can optionally set descriptions to the mappings (used in the desc parameter of
+              -- nvim_buf_set_keymap) which plugins like which-key display
+              -- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              -- You can also use captures from other query groups like `locals.scm`
+              -- ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
             },
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+              ['@function.outer'] = 'v', -- charwise
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true or false
+            include_surrounding_whitespace = true,
           },
-          indent = {
-            enable = false
-          },
-        })
-    end
+        },
+      })
+  end
  }
