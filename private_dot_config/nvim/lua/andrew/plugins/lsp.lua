d4d0bf34cb9686e5fcfb5bc8ac9cf37e4ff02316
Andrew Farries <andrew.farries@xata.io>
2024-03-31 22:03:06 +0100

    Remove redundant keymap for formatting with LSP
    This is done on save by `conform-nvim` now.

--- private_dot_config/nvim/lua/andrew/plugins/lsp.lua
+++ private_dot_config/nvim/lua/andrew/plugins/lsp.lua
@@ -54,9 +54,11 @@ return {
         vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', '<leader>f', function()
-          vim.lsp.buf.format { async = true }
-        end, opts)
+
+        -- redundant when using conform
+        -- vim.keymap.set('n', '<leader>f', function()
+        --   vim.lsp.buf.format { async = true }
+        -- end, opts)
       end,
     })
   end,
