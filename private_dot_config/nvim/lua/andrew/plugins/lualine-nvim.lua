6738eb7ccb41fe558d6367396246a6abefe822f5
Andrew Farries <andrew.farries@xata.io>
2024-04-02 16:00:24 +0100

    Configure lualine separators

--- private_dot_config/nvim/lua/andrew/plugins/lualine-nvim.lua
+++ private_dot_config/nvim/lua/andrew/plugins/lualine-nvim.lua
@@ -1,11 +1,11 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'nightfly',
-      },
-    })
-  end
+  opts = {      
+    options = {
+      theme = 'nightfly',
+      component_separators = { left = "", right = "" },
+      section_separators = { left = '', right = ''},
+    },
+  },
 }
