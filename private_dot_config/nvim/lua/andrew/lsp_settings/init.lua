5b0e34a29bfd7ae7d713ed6b973481352229ea65
Andrew Farries <andrew.farries@xata.io>
2024-04-03 18:19:43 +0100

    Setup more gopls settings

--- private_dot_config/nvim/lua/andrew/lsp_settings/init.lua
+++ private_dot_config/nvim/lua/andrew/lsp_settings/init.lua
@@ -4,5 +4,23 @@
 return {
   gopls = {
     gofumpt = true,
+    experimentalPostfixCompletions = true,
+    staticcheck = true,
+    symbolScope = "workspace",
+    --
+    -- Inlay hints will be supported in nvim 0.10.
+    -- Turn them all on for now, but they don't show.
+    -- After 0.10 you will probably want to have a toggle
+    -- for these.
+    --
+    hints = {
+      compositeLiteralFields = true,
+      compositeLiteralTypes = true,
+      assignVariableTypes = true,
+      constantValues = true,
+      functionTypeParameters = true,
+      parameterNames = true,
+      rangeVariableTypes = true,
+    },
   },
 }
