8eb59201d6af6bba57770517572f1072694589cf
Andrew Farries <andrew.farries@xata.io>
2024-04-08 09:27:28 +0100

    Move telescope prompt to the top of the window

--- private_dot_config/nvim/lua/andrew/plugins/telescope.lua
+++ private_dot_config/nvim/lua/andrew/plugins/telescope.lua
@@ -7,6 +7,15 @@ return {
   },
   config = function()
     require('telescope').setup({
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+          },
+        },
+        sorting_strategy = "ascending",
+      },
       extensions = {
         fzf = {
           fuzzy = true,
