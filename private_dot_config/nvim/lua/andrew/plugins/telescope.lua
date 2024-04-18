9c6145e825abfe2d18006aecd408fda70b8f96ca
Andrew Farries <andrew.farries@xata.io>
2024-04-18 18:32:42 +0100

    Add keymaps for more Telescope pickers

--- private_dot_config/nvim/lua/andrew/plugins/telescope.lua
+++ private_dot_config/nvim/lua/andrew/plugins/telescope.lua
@@ -31,8 +31,13 @@ return {
 
     vim.keymap.set('n', '<c-p>', builtin.find_files, { desc = 'Telescope Find Files' })
     vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = 'Telescope Current Buffer Fuzzy Find' })
+
     vim.keymap.set('n', '<leader>rg', builtin.live_grep, { desc = 'Telescope Live Grep' })
+    vim.keymap.set('n', '<leader>gs', builtin.grep_string, { desc = 'Telescope Grep String' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope Help Tags' })
+    vim.keymap.set('n', '<leader>ch', builtin.command_history, { desc = 'Telescope Command History' })
+    vim.keymap.set('n', '<leader>sh', builtin.search_history, { desc = 'Telescope Search History' })
+    vim.keymap.set('n', '<leader>km', builtin.keymaps, { desc = 'Telescope Keymaps' })
 
     vim.keymap.set('n', '<leader><space>', function()
       builtin.buffers({ sort_lastused = true })
