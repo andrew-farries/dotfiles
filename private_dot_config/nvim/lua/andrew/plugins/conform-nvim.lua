5fc27bd29bd2a84f24996a594816fa385addbdf2
Andrew Farries <andrew.farries@xata.io>
2024-12-15 14:58:16 +0000

    Update conform settings
    Don't format by default, opt-in per file type.

--- private_dot_config/nvim/lua/andrew/plugins/conform-nvim.lua
+++ private_dot_config/nvim/lua/andrew/plugins/conform-nvim.lua
@@ -1,9 +1,20 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    -- Don't do any formatting by default, opt-in per file type
+    default_format_opts = {
+      lsp_format = "never",
+    },
+
+    -- Enable formatting for specific file types
+    formatters_by_ft = {
+      go    = { lsp_format = "prefer" },
+      gomod = { lsp_format = "prefer" },
+    },
+
+    -- Automatically format on save
     format_on_save = {
       timeout_ms = 500,
-      lsp_fallback = true,
     },
   }
 }
