01767d9100aee8950a303c65e320030620e661a8
Andrew Farries <andrew.farries@xata.io>
2025-07-04 14:08:49 +0100

    Add kubeconfig info to fish prompt

--- private_dot_config/fish/functions/fish_prompt.fish
+++ private_dot_config/fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt --description 'Write out the prompt'
-    echo -n -s (set_color green) (kubeconfig_env) (set_color cyan) '[' (set_color $fish_color_cwd)(basename (dirs))(set_color cyan) '] ' (git_info) (set_color normal) '> '
+    echo -n -s (set_color cyan) '[' (set_color $fish_color_cwd)(basename (dirs))(set_color cyan) '] ' (kubeconfig_env) (git_info) (set_color normal) '> '
 end
--- private_dot_config/fish/functions/kubeconfig_env.fish
+++ private_dot_config/fish/functions/kubeconfig_env.fish
@@ -1,6 +1,5 @@
 function kubeconfig_env --description "Print a message if KUBECONFIG is set"
   if set -q KUBECONFIG
-    echo -n (set_color blue)\ue81d(set_color cyan)
-    echo -n ' '
+    echo -n (set_color cyan)'['(set_color blue)\ue81d(set_color normal)' '(set_color 4FB3D9)(kubectl config current-context)(set_color cyan)'] '
   end
 end
